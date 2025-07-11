9982fee6706661ff3172926ba788718e1ab217ec
SonaliV14 <sonalivarshney1404@gmail.com>
2025-07-11 22:23:47 +0530

    Learning zod and global catches

--- middlwares/zod.js
+++ middlwares/zod.js
@@ -1,8 +1,23 @@
- const express = require("express");
- const app=express();
- const zod=require("zod");
+const express = require("express");
+const app = express();
+const zod = require("zod");
 
- const schema=zod.array(zod.number());
+const schema = zod.array(zod.number());
 
- app.use(express.json());
- 
+app.use(express.json());
+
+app.post("/health-checkup", function (req, res) {
+    const kidneys = req.body.kidneys;
+    const response = schema.safeParse(kidneys);
+    if (!response.success) {
+        res.status(411).json({
+            msg: "Error was there"
+        })
+    } else {
+        res.send({
+            response
+        })
+    }
+})
+
+app.listen(3000); 
