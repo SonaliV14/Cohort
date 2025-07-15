3c32b7aef54964933b4d9fabf327d3726e374f6f
SonaliV14 <sonalivarshney1404@gmail.com>
2025-07-15 23:47:30 +0530

    Integrating mongoose library

---
+++ database/mongoose.js
@@ -0,0 +1,46 @@
+const express=require("express");
+const jwt= require("jsonwebtoken");
+const mongoose= require("mongoose");
+const jwtPassword="123456";
+
+mongoose.connect(
+    "mongodb://127.0.0.1:27017/myDatabase",
+);
+
+const User = mongoose.model("User",{
+    name: String,
+    username: String,
+    password: String
+});
+
+app.post("signup", async function(req, res){
+    const name=req.body.name;
+    const username= req.body.username;
+    const password= req.body.password;
+
+    const existingUser = await User.findOne({ email: username});
+    if(existingUser){
+        return res.status(400).send("Username already exists !");
+    }
+});
+
+const user= new User({
+    name: name,
+    username: username,
+    password: password
+});
+
+user.save()
+.then(() => console.log("User details saved successfully!"))
+.catch((err) => console.log("Error saving user:", err));
+
+res.json({
+    msg: "User created successfully!"
+})
+
+const app= express();
+app.use(express.json());
+
+app.listen(3000);
+
+
